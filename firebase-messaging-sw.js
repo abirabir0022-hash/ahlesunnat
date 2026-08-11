924d879a9775483aaf2c6fee2aeb2699f957a3cb
abirabir0022-hash <abirabir0022@gmail.com>
2026-08-11 15:44:54 +0600

    Improve Firebase background message handling
    Refactor background message handling and notification options.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,11 +1,15 @@
+// Firebase App library
 importScripts(
   "https://www.gstatic.com/firebasejs/12.17.1/firebase-app-compat.js"
 );
 
+// Firebase Messaging library
 importScripts(
   "https://www.gstatic.com/firebasejs/12.17.1/firebase-messaging-compat.js"
 );
 
+
+// Firebase configuration
 firebase.initializeApp({
   apiKey: "AIzaSyCPRWpUI98Ggr4rrlC5yqXsaaAJIkCNp-0",
   authDomain: "ahle-sunnat-islamic-qa.firebaseapp.com",
@@ -15,23 +19,38 @@ firebase.initializeApp({
   appId: "1:58263470817:web:31e14a98ebbd00f9c26da0"
 });
 
+
+// Firebase Cloud Messaging চালু
 const messaging = firebase.messaging();
 
+
+// Website background/closed থাকলে
+// এই অংশ notification দেখাবে
 messaging.onBackgroundMessage((payload) => {
-  console.log("Background message received:", payload);
+
+  console.log(
+    "Background message received:",
+    payload
+  );
 
   const notificationTitle =
-    payload.notification?.title || "আহলে সুন্নাত";
+    payload.notification?.title ||
+    "আহলে সুন্নাত";
 
   const notificationOptions = {
+
     body:
       payload.notification?.body ||
       "নতুন একটি বার্তা এসেছে।",
-    icon: "./icon-192.png"
+
+    icon:
+      "./icon-192.png"
   };
+
 
   self.registration.showNotification(
     notificationTitle,
     notificationOptions
   );
+
 });
